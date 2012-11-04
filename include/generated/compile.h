8b16ee2c5e058b7d6daeb56e68f451501fd12f63
Fagulhas <ricardo.pinho.rp@gmail.com>
2012-11-04 14:21:57 +0000

    Fix my own Derp, Revert "bcm4330: Fix some variable derps"
    This reverts commit 5e2e3213bcb5c3d9e2aee24671a14083baf0fadc.

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Sun Nov 4 13:12:35 WET 2012"
-#define LINUX_COMPILE_BY "fagulhas"
-#define LINUX_COMPILE_HOST "jellybean"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
