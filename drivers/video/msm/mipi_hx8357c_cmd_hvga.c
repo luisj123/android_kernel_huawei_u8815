fb8049d13a22b3caa0bf82d7ff89d19a55afccda
Daz Jones <yuki@thebronasium.com>
2012-12-27 13:16:26 +0000

    set refx100 to 6500
    Conflicts:
    
    	drivers/video/msm/mipi_hx8357c_cmd_hvga.c
    	drivers/video/msm/mipi_hx8369a_cmd_wvga.c
    	drivers/video/msm/mipi_nt35560_cmd_fwvga.c
    	drivers/video/msm/mipi_rsp61408_cmd_wvga.c

--- drivers/video/msm/mipi_hx8357c_cmd_hvga.c
+++ drivers/video/msm/mipi_hx8357c_cmd_hvga.c
@@ -187,9 +187,8 @@ static int __init mipi_cmd_hx8357c_hvga_init(void)
 		pinfo->bl_max = 255;
 		pinfo->bl_min = 30;
 		pinfo->fb_num = 2;
-        pinfo->clk_rate = 250000000;/* 60fps */
-		pinfo->lcd.refx100 = 6000; /* adjust refx100 to prevent tearing */
-
+		pinfo->clk_rate = 250000000;/* 60fps */
+		pinfo->lcd.refx100 = 6500; /* adjust refx100 to prevent tearing */
 		pinfo->mipi.mode = DSI_CMD_MODE;
 		pinfo->mipi.dst_format = DSI_CMD_DST_FORMAT_RGB888;
 		pinfo->mipi.vc = 0;
